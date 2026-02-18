7f5ccb0c533471bed64e40bafa15bde49b863df1
Yuriy Bandura <yurabandura98@gmail.com>
2026-02-18 23:00:37 +0200

    b

--- bot/modules/services.py
+++ bot/modules/services.py
@@ -27,12 +27,6 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
             "Данный бот умеет скачивать торренты/прямые линки, умеет скачивать с YouTube/VK/OK и многих-многих других ресурсов.\n\n⚠️ ⚠️ Если хотите получить доступ к боту — оформите, пожалуйста, подписку.\n Подписка: https://t.me/tribute/app?startapp=sMms",
             reply_markup,
         )
-    buttons = ButtonMaker()
-    buttons.url_button(
-        "Подписка ->", "https://t.me/tribute/app?startapp=sMms"
-    )
-    buttons.url_button("Помощь —", "https://t.me/ksi_fbot")
-    reply_markup = buttons.build_menu(2)
 
 
 @new_task
